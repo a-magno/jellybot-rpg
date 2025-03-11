042c83c8e7122f6d09327bf7cdc99251b5e4524b
a-magno <48924266+a-magno@users.noreply.github.com>
2025-03-10 22:14:49 -0300

    new: added new dice parser

--- src/functions/index.ts
+++ src/functions/index.ts
@@ -1,1 +1,29 @@
 // export functions here
+import { DiceRoll, DiceRoller } from "@dice-roller/rpg-dice-roller";
+
+const roller = new DiceRoller();
+
+interface DiceRollResult {
+    message? : string,
+    total?: number,
+    ephemeral? : boolean
+}
+
+export function rollDice( input : string ) : DiceRollResult {
+    roller.clearLog();
+    try{
+        const roll : DiceRoll | DiceRoll[] = roller.roll(input);
+        console.log(roller.output);
+
+        return {
+            message : roll.toString(),
+            total : roller.total
+        }
+    }catch(err){
+        // console.log(err)
+        return {
+            message : "There's been a syntax error. See ``/help`` for any questions!",
+            ephemeral : true
+        }
+    }
+}
